33c2c10e947d038944785fafa59121f405b18a5d
David Yahalomi <davidyaha@gmail.com>
2013-09-23 19:29:35 -0400

    Changed the name of the angular resources script to a more upfront name..

--- web-client/assets/js/connection.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var connections = angular.module('connection', ['ngResource']).
-    factory('Torrents', function ($resource) {
-        return $resource('/torrentList', {});
-    });
-
-connections.factory('Options', function ($resource) {
-    return $resource('/options', {});
-});
